16b00f3f825fdb6f39214f4a138ec6c5d1794c8b
Ibrahim <aitkazindev@gmail.com>
2022-09-20 11:43:08 +0900

    Changed Structure of program added Category model

---
+++ ShoppingApp/Helpers/Constants.swift
@@ -0,0 +1,44 @@
+//
+//  Constants.swift
+//  ShoppingApp
+//
+//  Created by GreyResearch_design on 2022/09/20.
+//
+
+import Foundation
+import Foundation
+
+//IDS and Keys
+public let kFILEREFERENCE = "gs://shopping-a9530.appspot.com"
+
+//Firebase Headers
+public let kUSER_PATH = "User"
+public let kCATEGORY_PATH = "Category"
+public let kITEMS_PATH = "Items"
+public let kBASKET_PATH = "Basket"
+
+
+//Category
+public let kNAME = "name"
+public let kIMAGENAME = "imageName"
+public let kOBJECTID = "objectId"
+
+//Item
+public let kCATEGORYID = "categoryId"
+public let kDESCRIPTION = "describtion"
+public let kPRICE = "price"
+public let kIMAGELINKS = "imageLinks"
+
+//Basket
+public let kOWNERID = "ownerId"
+public let kITEMIDS = "itemIds"
+
+//Users
+public let kEMAIL = "email"
+public let kFIRST_NAME = "firstName"
+public let kLASTNAME = "lastName"
+public let kFULLNAME = "fullName"
+public let kCURRENTUSER = "currentUser"
+public let kFULLADDRESS = "fullAddress"
+public let kONBOARD = "onBoard"
+public let kPURCHASEDITEMS = "purchasedItemsIds"
