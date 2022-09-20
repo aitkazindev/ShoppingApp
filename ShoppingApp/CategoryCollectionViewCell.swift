df6bee2c7908fbc2ff4fa2bf9384b2a73cb66c46
Ibrahim <aitkazindev@gmail.com>
2022-09-20 10:47:50 +0900

    Tab bar controller added

---
+++ ShoppingApp/CategoryCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryCollectionViewCell.swift
+//  ShoppingApp
+//
+//  Created by GreyResearch_design on 2022/09/20.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    
+}
