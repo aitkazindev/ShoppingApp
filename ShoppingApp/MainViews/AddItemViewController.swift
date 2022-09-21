b245117fcff8b514e0e907ab1d999e426bd19230
Ibrahim <aitkazindev@gmail.com>
2022-09-21 16:53:06 +0900

    Dismissing keyboard and checking text fields for adding new item

--- ShoppingApp/MainViews/AddItemViewController.swift
+++ ShoppingApp/MainViews/AddItemViewController.swift
@@ -6,9 +6,46 @@
 //
 
 import UIKit
-
+import InstantSearch
+import JGProgressHUD
+import NVActivityIndicatorView
 class AddItemViewController: UIViewController {
 
+    //MARK: IBOutlets
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var priceTextField: UITextField!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+    //MARK: Vars
+       var category: Category!
+       //var gallery: GalleryController!
+       var hud = JGProgressHUD(style: .dark)
+       var activityIndicator: NVActivityIndicatorView?
+       var itemImages: [UIImage?] = []
+    
+    //MARK: IBActions
+    @IBAction func doneBarButtonItemPressed(_ sender: Any) {
+        dismissKeyboard()
+        if fieldsAreCompleted() {
+//            saveToFirebase()
+        }else{
+            print("Error are fields are required")
+//          self.hud.textLabel.text = "All fields are required"
+//            self.hud.indicatorView = JGProgressHUDIndicatorView()
+//            self.hud.show(in: self.view)
+//            self.hud.dismiss(afterDelay: 2.0)
+        }
+    }
+        
+    @IBAction func cameraButtonPressed(_ sender: Any) {
+        //showImageGallery()
+    }
+    
+    @IBAction func backgroundTapped(_ sender: Any) {
+            dismissKeyboard()
+        }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +53,14 @@ class AddItemViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: Helper functions
+    
+    private func dismissKeyboard(){
+        self.view.endEditing(false)
     }
-    */
+    
+    private func fieldsAreCompleted() -> Bool{
+        return (titleTextField.text != "" && priceTextField.text != "" && descriptionTextView.text != "")
+    }
 
 }
